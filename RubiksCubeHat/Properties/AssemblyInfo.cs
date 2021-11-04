f26110112286ddec4c541eb7e1db398ba2efa786
Rodrigo Soriano <rodrigo_skate9@hotmail.com>
2021-11-03 22:01:40 -0600

    Add project files.

---
+++ RubiksCubeHat/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// The title of your mod, as displayed in menus
+[assembly: AssemblyTitle("|GREEN|R|WHITE|u|BLUE|b|ORANGE|i|YELLOW|k|RED|'s |WHITE|Cube Hat")]
+
+// The author of the mod
+[assembly: AssemblyCompany("Bsnollie")]
+
+// The description of the mod
+[assembly: AssemblyDescription("Rubik's Cube Hat!")]
+
+// The mod's version
+[assembly: AssemblyVersion("1.0.2.1")]
---
+++ RubiksCubeHat/RubiksCubeHat.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace DuckGame.RubiksCubeHat
+{
+    public class RubiksCubeHat : ClientMod
+    {
+        public override Priority priority
+        {
+            get
+            {
+                return Priority.HatPack;
+            }
+        }
+
+        protected override void OnPostInitialize()
+        {
+            Team[] teams = new Team[]
+            {
+                Team.Deserialize(GetPath<RubiksCubeHat>("hats/Rubiks S.png")),
+                Team.Deserialize(GetPath<RubiksCubeHat>("hats/Rubiks S+.png")),
+                Team.Deserialize(GetPath<RubiksCubeHat>("hats/Rubiks.png")),
+                Team.Deserialize(GetPath<RubiksCubeHat>("hats/Rubiks+.png"))
+            };
+
+            foreach (Team team in teams)
+            {
+                if (team != null)
+                {
+                    Teams.AddExtraTeam(team);
+                }
+            }
+
+            base.OnPostInitialize();
+        }
+    }
+}
